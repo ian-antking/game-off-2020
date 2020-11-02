8a0ebc1fd680d1c376759d3bfa7380d0baed115b
ian-antking <ian.antking@gmail.com>
2020-11-02 21:18:01 +0000

    use center utility class and remove unused code

--- src/scenes/titlescene.js
+++ src/scenes/titlescene.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import Align from '../utils/align';
 
 export default class TitleScene extends Phaser.Scene {
   constructor() {
@@ -9,21 +10,21 @@ export default class TitleScene extends Phaser.Scene {
   }
 
   create() {
-    const { width, height } = this.scale;
-    this.logo = this.add.image((width / 2), (height / 2) - 50, 'logo');
+    this.logo = this.add.image(0, 0, 'logo');
+    Align.center(this.logo, this);
+    this.logo.y -= 50;
     this.logo.setScale(0.75);
-    
-    const titleText = this.make.text({
-      x: width / 2,
-      y: height / 2 + 100,
+
+    this.titleText = this.make.text({
       text: 'Press Spacebar',
       style: {
         font: '20px future-thin',
         fill: '#ffffff',
       },
     });
-    titleText.setOrigin(0.5, 0.5);
-    this.spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.titleText.setOrigin(0.5, 0.5);
+    Align.center(this.titleText, this);
+    this.titleText.y += 100;
 
     this.input.keyboard.on('keydown-SPACE', () => {
       this.scene.start('Game');
