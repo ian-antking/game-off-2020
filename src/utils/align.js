88bd011848ef087a259ad41f4fe9494384c618d4
ian-antking <ian.antking@gmail.com>
2020-11-02 19:46:24 +0000

    add utility classes

---
+++ src/utils/align.js
@@ -0,0 +1,25 @@
+export default class Align
+{
+  static scaleToGameW(object, percentage, game) {
+    object.displayWidth=game.scale.width*percentage;
+    object.scaleY=object.scaleX;
+  }
+
+  static centerH(object, game) {
+    object.x=game.scale.width/2-object.displayWidth/2;
+  }
+	
+  static centerV(object, game) {
+    object.y=game.scale.height/2-object.displayHeight/2;
+  }
+
+  static center2(object, game) {
+    object.x=game.scale.width/2-object.displayWidth/2;
+    object.y=game.scale.height/2-object.displayHeight/2;
+  }
+
+  static center(object, game) {
+    object.x=game.scale.width/2;
+    object.y=game.scale.height/2;
+  }
+}
---
+++ src/utils/alignGrid.js
@@ -0,0 +1,70 @@
+export default class AlignGrid {
+  constructor({ rows, columns, height, width, scene }) {
+    if (!scene)
+    {
+      // eslint-disable-next-line no-console
+      console.log('missing scene');
+      return;
+    }
+    this.scene = scene;
+    this.rows = rows || 5;
+    this.columns = columns || 5;
+    this.height = height || scene.scale.height;
+    this.width = width || this.scene.scale.width;
+
+    //cell width
+    this.cw=width/columns;
+    //cell height
+    this.ch=height/rows;
+  }
+
+  show()
+  {
+    this.graphics=this.scene.add.graphics();
+    this.graphics.lineStyle(2,0xff0000);
+
+    for (let i = 0; i < this.config.width; i+=this.cw) {
+      this.graphics.moveTo(i,0);
+      this.graphics.lineTo(i,this.config.height);
+    }
+
+    for (let i = 0; i < this.config.height; i+=this.ch) {
+      this.graphics.moveTo(0,i);
+      this.graphics.lineTo(this.config.width,i);
+    }
+		
+    this.graphics.strokePath();
+  }
+
+  placeAt(xx,yy,obj)
+  {
+    //calc position based upon the cellwidth and cellheight
+    const x2=this.cw*xx+this.cw/2;
+    const y2=this.ch*yy+this.ch/2;
+
+    obj.x=x2;
+    obj.y=y2;
+  }
+  placeAtIndex(index,obj)
+  {
+    const yy=Math.floor(index/this.columns);
+    const xx=index-(yy*this.columns);
+
+    this.placeAt(xx,yy,obj);
+
+  }
+  showNumbers()
+  {
+    this.show();
+    let count=0;
+    for (let i = 0; i < this.config.rows; i++) {
+      for(let j=0;j<this.config.cols;j++)
+      {
+        const numText=this.scene.add.text(0,0,count,{color:'#ff0000'});
+        numText.setOrigin(0.5,0.5);
+        this.placeAtIndex(count,numText);
+        count++;
+      }
+    }
+  }
+}
