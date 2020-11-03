913f5dacf322b5628629ba99e7c8400005617952
ian-antking <ian.antking@gmail.com>
2020-11-03 22:24:03 +0000

    set up deployment scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'project.bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname),
-    publicPath: '/build',
+    publicPath: '/dist',
     compress: true,
     port: 8080
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'assets', to: 'assets' },
+        'index.html'
+      ]
+    }),
+    new HtmlWebpackPlugin({ 
+      template: './src/index.html'
+    })
+  ]
 };
