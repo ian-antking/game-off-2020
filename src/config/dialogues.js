d579e8ed44c2685defcf42aa97f2e62f41b7d7bb
ian-antking <ian.antking@gmail.com>
2020-11-17 19:56:50 +0000

    build story sequence, with random date order

--- src/config/dialogues.js
+++ src/config/dialogues.js
@@ -1,5 +1,18 @@
-export default [
+const dateDialogues = [
+  'mars',
+  'earth'
+];
+
+const randomDate = () => {
+  const date = dateDialogues.splice(Math.floor(Math.random() * dateDialogues.length), 1);
+  return date[0];
+};
+
+const dialogues = [
   'opening',
   'intro',
-  'mars',
+  randomDate(),
+  randomDate(),
 ];
+
+export default dialogues;
